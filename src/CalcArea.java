ec5cc8b52f0d165f72f6ffb4f69ad447e92a4b6f
omar-basheer <112007297+omar-basheer@users.noreply.github.com>
2023-03-14 10:42:27 +0000

    updates

--- src/CalcArea.java
+++ src/CalcArea.java
@@ -9,7 +9,7 @@ public class CalcArea {
     }
 
     public static double[][] findEarthQuakeArea(double x1, double y1, double x2, double y2, double x3, double y3, double sp1, double sp2){
-        double area1, area2, area3, tri_area;
+        double area1, area2, area3, tri_area, total;
         double[][] triangle = new double[0][];
         tri_area = findArea(x1,y1,x2,y2,x3,y3);
         System.out.println("triangle area: " + tri_area);
@@ -19,11 +19,14 @@ public class CalcArea {
         System.out.println("area2: " + area2);
         area3 = findArea(sp1,sp2,x2,y2,x3,y3);
         System.out.println("area3: " + area3);
+        total = area1+area2+area3;
+        System.out.println("area1+area2+area3: " + total);
 
         if(area1+area2+area3 == tri_area){
             triangle = new double[][]{{x1,y1},{x2,y2},{x3,y3}};
             System.out.println("safe triangle: " + Arrays.deepToString(triangle));
         }
+        System.out.println(" ");
         return triangle;
     }
 }
--- src/FindSafeSurrounding.java
+++ src/FindSafeSurrounding.java
@@ -20,28 +20,27 @@ public class FindSafeSurrounding {
 
         Set<Double> keySet = distances.keySet();
         ArrayList<Double> all_distances = new ArrayList<>(keySet);
-        double min_dist1 = all_distances.get(0);
-        double min_dist2 = all_distances.get(0);
-        int index1 = 0, index2 = 0;
+        double min_dist = all_distances.get(0);
+        double[] close_point1 = new double[0];
+        double[] close_point2 = new double[0];
+        int min_index = 0;
 
-        for(int i=0; i<all_distances.size(); i++){
-            if(all_distances.get(i) < min_dist1){
-                min_dist1 = all_distances.get(i);
-                index1 = i;
+        for(int i=0; i<2; i++){
+            for(int j=0; j<all_distances.size(); j++){
+                if(all_distances.get(j) < min_dist){
+                    min_dist = all_distances.get(j);
+                    min_index = j;
+                }
+            }
+            if(i==1){
+                close_point2 = new double[]{distances.get(min_dist)[1][0], distances.get(min_dist)[1][1]};
+            }else {
+                close_point1 = new double[]{distances.get(min_dist)[1][0], distances.get(min_dist)[1][1]};
+                all_distances.remove(min_index);
+                min_dist = all_distances.get(0);
             }
         }
-        all_distances.remove(index1);
 
-        for(int i=0; i<all_distances.size(); i++){
-            if(all_distances.get(i) < min_dist2){
-                min_dist2 = all_distances.get(i);
-                index2 = i;
-            }
-        }
-
-        // get the closest points
-        double[] close_point1 = new double[]{distances.get(min_dist1)[1][0], distances.get(min_dist1)[1][1]};
-        double[] close_point2 = new double[]{distances.get(min_dist2)[1][0], distances.get(min_dist2)[1][1]};
         System.out.println(Arrays.toString(close_point1));
         System.out.println(Arrays.toString(close_point2));
 
@@ -49,52 +48,39 @@ public class FindSafeSurrounding {
         Collection<double[][]> values = FindSafeTriangle.triangles.values();
         ArrayList<double[][]> allValues = new ArrayList<>(values);
 
+        int count = 0;
         for(int k=0; k<allValues.size(); k++){
-            System.out.println("test");
+//            System.out.println("test");
             double[][] temp = allValues.get(k);
+            double[] close_point3;
             for(int i=0; i<temp.length; i++){
+                close_point3 = safe_point;
                 if((Arrays.equals(temp[0], close_point1) && Arrays.equals(temp[1], close_point2))){
-                    double[] close_point3 = temp[2];
-                    // calc area helper
-                    CalcArea.findEarthQuakeArea(close_point1[0],close_point1[1],close_point2[0],close_point2[1],close_point3[0],close_point3[1],safe_point[0],safe_point[1]);
+                    close_point3 = temp[2];
                 }
                 else if((Arrays.equals(temp[2], close_point2) && Arrays.equals(temp[1], close_point1))){
-                    double[] close_point3 = temp[0];
-                    // calc area helper
-                    CalcArea.findEarthQuakeArea(close_point1[0],close_point1[1],close_point2[0],close_point2[1],close_point3[0],close_point3[1],safe_point[0],safe_point[1]);
+                    close_point3 = temp[0];
                 }
                 else if((Arrays.equals(temp[0], close_point2) && Arrays.equals(temp[2], close_point1))){
-                    double[] close_point3 = temp[1];
-                    // calc area helper
-                    CalcArea.findEarthQuakeArea(close_point1[0],close_point1[1],close_point2[0],close_point2[1],close_point3[0],close_point3[1],safe_point[0],safe_point[1]);
+                    close_point3 = temp[1];
                 }
                 else if((Arrays.equals(temp[1], close_point2) && Arrays.equals(temp[2], close_point1))){
-                    double[] close_point3 = temp[0];
-                    // calc area helper
-                    CalcArea.findEarthQuakeArea(close_point1[0],close_point1[1],close_point2[0],close_point2[1],close_point3[0],close_point3[1],safe_point[0],safe_point[1]);
+                    close_point3 = temp[0];
                 }
                 else if((Arrays.equals(temp[2], close_point2) && Arrays.equals(temp[0], close_point1))){
-                    double[] close_point3 = temp[1];
-                    // calc area helper
-                    CalcArea.findEarthQuakeArea(close_point1[0],close_point1[1],close_point2[0],close_point2[1],close_point3[0],close_point3[1],safe_point[0],safe_point[1]);
+                    close_point3 = temp[1];
                 }
                 else if((Arrays.equals(temp[0], close_point2) && Arrays.equals(temp[1], close_point1))){
-                    double[] close_point3 = temp[2];
-                    // calc area helper
+                    close_point3 = temp[2];
+                }
+                if(close_point3 != safe_point){
                     CalcArea.findEarthQuakeArea(close_point1[0],close_point1[1],close_point2[0],close_point2[1],close_point3[0],close_point3[1],safe_point[0],safe_point[1]);
+                    count++;
                 }
-                else{
-                    System.out.println("warning: person is not located within any safe triangle");
-                }
-
             }
         }
-
+        if(count == 0){
+            System.out.println("warning: person is not located within any safe triangle");
+        }
     }
-
-    public static void main(String[]args){
-
-
-    }
-
 }
